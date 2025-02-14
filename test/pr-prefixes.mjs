2c974b25e5eac4e5c60e91d5d45aa24383f3422f
Jacob Smith <3012099+JakobJingleheimer@users.noreply.github.com>
2025-02-14 12:21:25 +0100

    !fixup: dammit biome

--- test/pr-prefixes.mjs
+++ test/pr-prefixes.mjs
@@ -3,4 +3,11 @@ export const SCOPE_RGX = /^\w+\([\w\-\d]*(?:,[\w\d\-]*)*\): /;
 /**
  * The subset of conventional commit prefixes used in this project.
  */
-export const SUPPORTED_PREFIXES = ['doc', 'dep', 'fix', 'feat', 'setup', 'test'];
+export const SUPPORTED_PREFIXES = [
+	'doc',
+	'dep',
+	'fix',
+	'feat',
+	'setup',
+	'test',
+];
