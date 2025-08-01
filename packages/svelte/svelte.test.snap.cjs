b1824c362c7848515a248f1d6b840624487c4e46
Augustin Mauroy <97875033+AugustinMauroy@users.noreply.github.com>
2025-08-01 13:05:21 +0200

    update snapshots

--- packages/svelte/svelte.test.snap.cjs
+++ packages/svelte/svelte.test.snap.cjs
@@ -1,11 +1,11 @@
 exports[`svelte (e2e) > should work with \`--import\` 1`] = `
-"function _unknown_($$anchor) {\\n\\tlet name = 'world';\\n\\tvar main = root();\\n\\tvar h1 = $.child(main);\\n\\n\\th1.textContent = \`Hello \${name ?? ''}!\`;\\n\\t$.reset(main);\\n\\t$.append($$anchor, main);\\n}\\n"
+"function _unknown_($$anchor) {\\n\\tlet name = 'world';\\n\\tvar main = root();\\n\\tvar h1 = $.child(main);\\n\\n\\th1.textContent = 'Hello world!';\\n\\t$.reset(main);\\n\\t$.append($$anchor, main);\\n}\\n"
 `;
 
 exports[`svelte (e2e) > should work with \`--loader\` 1`] = `
-"function _unknown_($$anchor) {\\n\\tlet name = 'world';\\n\\tvar main = root();\\n\\tvar h1 = $.child(main);\\n\\n\\th1.textContent = \`Hello \${name ?? ''}!\`;\\n\\t$.reset(main);\\n\\t$.append($$anchor, main);\\n}\\n"
+"function _unknown_($$anchor) {\\n\\tlet name = 'world';\\n\\tvar main = root();\\n\\tvar h1 = $.child(main);\\n\\n\\th1.textContent = 'Hello world!';\\n\\t$.reset(main);\\n\\t$.append($$anchor, main);\\n}\\n"
 `;
 
 exports[`svelte (e2e) > should work with \`module.register\` 1`] = `
-"function _unknown_($$anchor) {\\n\\tlet name = 'world';\\n\\tvar main = root();\\n\\tvar h1 = $.child(main);\\n\\n\\th1.textContent = \`Hello \${name ?? ''}!\`;\\n\\t$.reset(main);\\n\\t$.append($$anchor, main);\\n}\\n"
+"function _unknown_($$anchor) {\\n\\tlet name = 'world';\\n\\tvar main = root();\\n\\tvar h1 = $.child(main);\\n\\n\\th1.textContent = 'Hello world!';\\n\\t$.reset(main);\\n\\t$.append($$anchor, main);\\n}\\n"
 `;
